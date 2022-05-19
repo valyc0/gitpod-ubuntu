a286af1cbd6b6686d66b6d87cf8b501737846496
valyc0 <valyc0@gmail.com>
2022-05-19 20:08:46 +0000

    mod

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ echo "ubuntu:ubuntu:N" >> CREATEUSERS.TXT
 docker run -dit  \
           --name=ubuntu20.04 \
 		  --privileged=true \
-           -p 338s9:3389 \
+           -p 3389:3389 \
            -p 2222:22 \
            -e TZ="Europe/Rome" \
            -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
